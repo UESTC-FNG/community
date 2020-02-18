3e8e255bca0645236ef5a4e2df3bbccf820472a3
FNG <784321631@qq.com>
2020-02-18 18:22:31 +0800

    title click

--- src/main/java/com/fng/controller/AuthorizedController.java
+++ src/main/java/com/fng/controller/AuthorizedController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
@@ -70,7 +71,16 @@ public class AuthorizedController {
             //重新登陆
             return "redirect:/";
         }
+    }
 
-
+    @GetMapping("/logOut")
+    public String logOut(HttpServletRequest request,
+                         HttpServletResponse response){
+        request.getSession().removeAttribute("user");
+        //重新创建新cook值为null
+        Cookie cookie=new Cookie("token",null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
     }
 }
